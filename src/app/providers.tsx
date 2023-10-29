b9ce19d1b825fd179148670bd3b935bf4e822dcc
light-planck <seseifunc16@gmail.com>
2023-10-30 00:28:59 +0900

    chore: nextui

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { NextUIProvider } from "@nextui-org/react"
+import { FC, ReactNode } from "react"
+
+interface Props {
+  children: ReactNode;
+}
+
+export const Providers: FC<Props> = ({ children }) => {
+  return (
+    <NextUIProvider>
+      { children}
+    </NextUIProvider>
+  )
+}
